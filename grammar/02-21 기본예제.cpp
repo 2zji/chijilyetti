dcac57d09faae0cb639c094297c5fdbc991bd8ee
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-03-21 11:56:11 +0900

    c++수정

--- grammar/02-21 기본예제.cpp
+++ grammar/02-21 기본예제.cpp
@@ -1,6 +1,10 @@
 ﻿#include <iostream>
+//이름공간, std::를 안해도 된다
+using namespace std;
+
 int main(void) {
-	//cout : 출력객체
+	//cout : 출력객체 endl : 개행객체
 	std::cout << "Hello World" << std::endl;
+	std::cout << "집가고시포유ㅠ" << std::endl;
 	return 0;
 } 
