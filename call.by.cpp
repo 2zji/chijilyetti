e914a616f1cdd4f2b8df57fbb9eec260393cf3f9
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-03-26 15:41:13 +0900

    call by pointer

--- call.by.cpp
+++ call.by.cpp
@@ -3,15 +3,17 @@
 //이름공간, std::를 안해도 된다
 using namespace std;
 
-void call_by_value(int val) {
+void call_by_value(int* val) {
 	//새로운 지역변수 val을 만들고 가기에 10을 넣음
-	val = 10;
+	//val거 가르키고 있는 대상의 값을 바꿈
+	*val = 10;
+	
 }
 
 int main(void) {
 	int num = 4;
 	//num의 값은 바뀌지 않는다
-	call_by_value(num);
+	call_by_value(&num);
 	cout << num;
 
 	return 0;
