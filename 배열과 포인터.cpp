162a3e8b7f864db1acfae0b1b472566af142a2e3
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-03-28 11:18:08 +0900

    포인터가 배열을 가리키는 원리

--- 배열과 포인터.cpp
+++ 배열과 포인터.cpp
@@ -12,6 +12,28 @@ int main(void) {
 	printf("%d %d \n", &str[0], &str[1]);
 	//	printf("%d %d \n", *(str+0), *(str+1));
 	//arr+0에는 차이가 없음
+
+	int i = 1;
+	char c = 'b';
+	double d = 3.0;
+
+	int* pi = &i;
+	char* pc = &c;
+	double* pd = &d;
+
+	//각각 4, 1, 8 차이
+	printf("%d %d %d \n", pi, pc, pd);
+	printf("%d %d %d \n", pi+1, pc+1, pd+1);
+//	printf("%d %d %d \n", sizeof(pi), sizeof(pc), sizeof(pd));
+
+	printf("%f %f %f\n", i, c, d);
+	printf("%f %f %f\n", i+1, c+1, d+1);
+
+	pi = arr;
+	printf("%d %d \n", pi + 1, arr + 1);
+	printf("%d %d \n", (*pi + 1), *(arr + 1));
+	printf("%d %d \n", pi[1], arr[1]);
+
 	return 0;
 
 }
