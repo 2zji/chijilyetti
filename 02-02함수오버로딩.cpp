fb71aaffeff3f841dcf1830cd8e9e8caa5359d46
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-04-04 11:18:50 +0900

    String대입1 (C언어 방식)

---
+++ 02-02함수오버로딩.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int mul(int a, int b) {
+	return a * b;
+}
+float mul(float a, float b) {
+	return a * b;
+}
+
+int main(void) {
+
+	cout << mul(2, 2) << endl;
+	cout << mul(2.2f, 2.2f) << endl;
+
+	return 0;
+}
