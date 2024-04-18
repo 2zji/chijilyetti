cf678aea9f407d551e2bcdef4be8c56c6f29f7fa
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-04-18 11:17:34 +0900

    03-02 소멸자

--- 03-02 생성자와 소멸자.cpp
+++ 03-02 생성자와 소멸자.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 class Student {
 public:
-	//생성자에서 동적할당을 한 메모리 공간을 지울 수 없다
+
 	Student(int hakbun, const char* name) : hakbun_(hakbun) {
 		int length = strlen(name);
 
 		//'/0'가 들어가기 위해 공간 하나 더 추가
 		name_ = new char[length + 1];
 
-		strcpy(name_, name);
+		strcpy(name_, name);	//미사용시 한자 출력
+	}
+
+	//소멸자: 객체가 소멸할 때(메모리에서 지위질 때)호출되는 함수
+	~Student(void) {
+		delete[]name_;
 	}
 
 	//클래스의 멤버를 출력
@@ -26,12 +31,8 @@ private:
 };
 
 int main(void) {
-//	Student junseok = Student(22, "YJS");
-//	junseok.show();
-
-	//TODO: 생성자에서 동적할당을 한 메모리 공간을 지울 수 없다
-	Student* junseok = new Student(22, "YJS");
-	junseok->show();
+	Student* junseok;
+	junseok = new Student(2207, "윤예지");
 	delete junseok;
 
 	return 0;
