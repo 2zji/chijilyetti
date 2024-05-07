bca7da962e5e00bdded0fb6b74e71276abec323f
Yeji Yun <142556812+2zji@users.noreply.github.com>
2024-05-07 16:04:14 +0900

    03-03 깊은복사

--- 03-02 생성자와 소멸자.cpp
+++ 03-02 생성자와 소멸자.cpp
@@ -14,9 +14,21 @@ public:
 		cout << "생성자 호출 완료" << endl;
 	}
 
+	//복사생성자(얕은 복사)
+	Student(const Student& rhs) : hakbun_(rhs.hakbun_), name_(rhs.name_) {
+		
+		int length = strlen(rhs.name_);
+		name_ = new char[length + 1];
+		strcpy[name_, rhs.name_];
+		
+		cout<<"복사생성자 호출"<< endl;
+	}
+
 	//소멸자: 객체가 소멸할 때(메모리에서 지위질 때)호출되는 함수
 	~Student(void) {
+		//HACK: 얕은 복사로 인하여 이미 제거된 공간을 또 제거하려고 해서 에러 발생
 		delete[]name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 
 	//클래스의 멤버를 출력
@@ -31,6 +43,15 @@ private:
 };
 
 int main(void) {
+
+	//일반생성자
+	Student stu1 = Student1(1111, "JWP");
+	stu1.show();
+	//복사생성자
+	Student stu2 = Student1(stu1);
+	stu2.show();
+
+	/*
 	int b = 2;
 	int ab = 4;
 	int& cc = ab;
@@ -43,7 +64,7 @@ int main(void) {
 
 	delete junseok;
 	cout << "배고파영" << endl;
-
+	*/
 
 	return 0;
 }
---
+++ test0429.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+	int sum(int a, int b);
+	void sum(int a, double b);
+}
